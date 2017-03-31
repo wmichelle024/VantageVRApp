b3a0a0c014c277fd9c069663cbf75aa42a4be767
user <wwj9527@gmail.com>
2017-03-30 23:04:27 -0700

    Inital upload of base project

---
+++ Assets/Scripts/DisplayInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class DisplayInfo : MonoBehaviour {
+	public TextMesh mainText;
+	public TextMesh additionalText;
+
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().enabled = false;
+		hideAllText ();
+	}
+	
+	public void showMainText() {
+		mainText.GetComponent<Renderer>().enabled = true;
+	}
+
+	public void showAdditionalText() {
+		additionalText.GetComponent<Renderer>().enabled = true;
+	}
+		
+	public void hideAllText() {
+		mainText.GetComponent<Renderer>().enabled = false;
+		additionalText.GetComponent<Renderer>().enabled = false;
+	}
+		
+}
